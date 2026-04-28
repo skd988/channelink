ebfe11f81021a956874c86809a7c6a144483f056
skd988 <skd988@gmail.com>
2026-04-28 15:14:35 +0300

    fixed tabbing and changed 4 spaces to 2 spaces, added a caching system to remember the channelink when the resolution changes

--- channelink.js
+++ channelink.js
@@ -6,102 +6,104 @@ const CHANNEL_NAME_SELECTOR = '.ytd-channel-name yt-formatted-string.ytd-channel
 
 const waitForElement = (target, selector) =>
 {
-    return new Promise((resolve, reject) => 
+  return new Promise((resolve, reject) => 
+  {
+    const selection = target.querySelector(selector);
+    if (selection)
+      return resolve(selection);
+    
+    const observer = new MutationObserver(() => 
     {
-        const selection = target.querySelector(selector);
-        if (selection)
-            return resolve(selection);
-        
-        const observer = new MutationObserver(() => 
-        {
-            const selection = target.querySelector(selector);
-            if(selection)
-            {
-                observer.disconnect();
-                return resolve(selection);
-            }
-        });
-        
-        observer.observe(target, {
-            childList: true,
-            subtree: true
-        });
+      const selection = target.querySelector(selector);
+      if(selection)
+      {
+        observer.disconnect();
+        return resolve(selection);
+      }
     });
+    
+    observer.observe(target, {
+      childList: true,
+      subtree: true
+    });
+  });
 };
 
 const getChannelUrl = async videoUrl =>
 {
-	return fetch(YOUTUBE_BASE_DOMAIN + videoUrl)
-	.then(res => res.ok? Promise.resolve(res.text()) : Promise.reject('Error:' + res.status))
-	.then(text => text
-                    .substring(text.indexOf(SEARCH_FOR_URL) + SEARCH_FOR_URL.length)
-                    .split('"')[0])
-    .catch(error => console.error(error));
+  return fetch(YOUTUBE_BASE_DOMAIN + videoUrl)
+  .then(res => res.ok? Promise.resolve(res.text()) : Promise.reject('Error:' + res.status))
+  .then(text => text
+          .substring(text.indexOf(SEARCH_FOR_URL) + SEARCH_FOR_URL.length)
+          .split('"')[0])
+  .catch(error => console.error(error, 'When tried to fetch: ', videoUrl));
 };
 
 const getVideoUrl = video =>
 {
-    return video?.querySelector('a')?.getAttribute('href');
-};
-
-const changeChannelNameToLink = (linkTag, name, videoUrl) =>
-{
-    linkTag.innerText = name;
-	
-    getChannelUrl(videoUrl)
-    .then(url => 
-        linkTag.setAttribute('href', url)
-    );
+  return video?.querySelector('a')?.getAttribute('href');
 };
 
 const setupUpdatingLink = video =>
-{	
-    waitForElement(video, CHANNEL_NAME_SELECTOR)
-    .then(channelNameElement =>
-    {
-		const name = channelNameElement.innerText;
+{
+  waitForElement(video, CHANNEL_NAME_SELECTOR)
+  .then(channelNameElement =>
+  {
+    const name = channelNameElement.innerText;
 
-        channelNameElement.setAttribute('has-link-only_', '');
-        const linkTag = document.createElement('a');
-        
-        linkTag.setAttribute('class', 'yt-simple-endpoint style-scope');
-		linkTag.setAttribute('style', '--yt-endpoint-color:var(--t4a6da19e16bf221a);--yt-endpoint-hover-color:var(--tffc2fd3a644f6275);');
-        changeChannelNameToLink(linkTag, 
-            name, getVideoUrl(video));
-			
-		channelNameElement.removeChild(channelNameElement.firstChild);
+    channelNameElement.setAttribute('has-link-only_', '');
+    const linkTag = document.createElement('a');
+    
+    linkTag.setAttribute('class', 'yt-simple-endpoint style-scope');
+    linkTag.setAttribute('style', '--yt-endpoint-color:var(--t4a6da19e16bf221a);--yt-endpoint-hover-color:var(--tffc2fd3a644f6275);');
+    
+    linkTag.innerText = name;
 
-		channelNameElement.prepend(linkTag);
-    });
-
-    const removalObservation = new MutationObserver(records =>
-    {
-        if(records.some(record => Array.from(record.removedNodes).some(removed => removed?.classList.contains('ytLockupViewModelHost'))))
+    const channelLink = video.getAttribute('channelLink');
+    if(!channelLink || channelLink === 'undefined')
+      getChannelUrl(getVideoUrl(video))
+      .then(url => 
         {
-            removalObservation.disconnect();
-            setupUpdatingLink(video);
+          linkTag.setAttribute('href', url)
+          video.setAttribute('channelLink', url);
         }
-    });
+      );
+    else
+      linkTag.setAttribute('href', channelLink)
 
-    removalObservation.observe(video, {
-        childList: true
-    });
+    channelNameElement.removeChild(channelNameElement.firstChild);
+
+    channelNameElement.prepend(linkTag);
+  });
+
+  const removalObservation = new MutationObserver(records =>
+  {
+    if(records.some(record => Array.from(record.removedNodes).some(removed => removed?.classList?.contains('ytLockupViewModelHost'))))
+    {
+      removalObservation.disconnect();
+      setupUpdatingLink(video);
+    }
+  });
+
+  removalObservation.observe(video, {
+    childList: true
+  });
 };
 
 const addLinksToWatchList = () =>
-{    
-    waitForElement(document, WATCH_LATER_SELECTOR)
-    .then(watchLater => 
-    {
-        const observer = new MutationObserver(records => 
-            records.forEach(record => record.addedNodes.forEach(setupUpdatingLink))
-        );
-        
-        observer.observe(watchLater, {
-            childList: true
-        });
-        watchLater.childNodes.forEach(setupUpdatingLink);
+{  
+  waitForElement(document, WATCH_LATER_SELECTOR)
+  .then(watchLater => 
+  {
+    const observer = new MutationObserver(records => 
+      records.forEach(record => record.addedNodes.forEach(setupUpdatingLink))
+    );
+    
+    observer.observe(watchLater, {
+      childList: true
     });
+    watchLater.childNodes.forEach(setupUpdatingLink);
+  });
 };
 
 addLinksToWatchList();
